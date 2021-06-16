b57bc12653d4f445a05cffa037b5678d657bd9a3
matsua <matsua12@gmail.com>
2021-06-16 11:14:49 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,18 +5,7 @@
     document.getElementById("popup").style.width = "0";
  }
 
-
-    
  document.getElementById("tt").onclick=function(){
     document.getElementById("popup").style.width = "250px";
     
  }
-
-
-
-
-
-
-
-
-  
